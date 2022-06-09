a3f9e93bf4a89b7f864c8a9cf48685a856056f4d
Sagar Saha <59755509+SagarSaha01@users.noreply.github.com>
2022-06-09 14:18:14 -0400

    added elements as a json file and updated command replying

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const { token } = require("./config.json");
-const command = ".play";
+const  elements  = require("./elements.json");
+const prefix = ".";
 
 client.once("ready", () => {
 	console.log("Botneisum Online!");
 })
 
 client.on("message", message => {
-	if (!message.content.startsWith(command)) return;
+	if (!message.content.startsWith(prefix)) return;
+
 	else {
-		message.channel.send("Working!");
+		// message.channel.send("Working!");
+		const command = message.content.substr(1, message.length);
+		message.channel.send(verify(command))
 	}
+	
 })
 
+function verify(userCommand) {
+	var reply;
+	if (userCommand.toLowerCase() == "play") {
+		reply = "Will start playing!";
+	} else if (userCommand.toLowerCase() == "help") {
+		reply = "Welcome to Botnesium! You can either play a game with me or get any information about any elements in the periodic table. \n\nTo play with me, type in **.play**. Or if you want to retrieve any information, type in **.info [followed by the element name]**. For example: `.info hydrogen` will return you with all the information about the Hydrogen element.";
+	} else if (userCommand.toLowerCase().substr(0, 4) == "info") {
+		reply = "Sending back with info";
+	}	else {
+		reply = "Command Not Found :( Please check for any typo or type in **.help** for more information. "
+	}
 
+	return reply;
+}
 
 client.login(token);
