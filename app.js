8d37ceebd3661582e28f39aff76e7a7f9bc0fa68
Sagar Saha <59755509+SagarSaha01@users.noreply.github.com>
2022-06-18 01:20:58 -0400

    updated .info to message embeds

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const {	Client,	Intents} = require('discord.js');
+/*
+	Name: Sagar Saha
+	Program Name: Botnesium
+	Program Desc: Botnesium is an interactive Discord Bot which is able to play with you and help you learn chemistry at the same time!
+	Date Started: June 6th, 2022
+	Last Edit Date: TBA
+*/
+
+const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 const { token } = require("./config.json");
 const elements = require("./elements.json");
@@ -15,7 +23,7 @@ client.on("message", message => {
 
 	else {
 		// message.channel.send("Working!");
-		const command = message.content.substring(1, message.length);
+		const command = message.content.substring(1, message.length).toLowerCase();
 		message.channel.send(verify(command))
 	}
 
@@ -23,13 +31,14 @@ client.on("message", message => {
 
 function verify(userCommand) {
 	var reply = "default";
-	if (userCommand.toLowerCase() == "play") {
+	if (userCommand == "play") {
+		play();
 		reply = "Will start playing!";
-	} else if (userCommand.toLowerCase() == "help") {
+	} else if (userCommand == "help") {
 		reply = "Welcome to Botnesium! You can either play a game with me or get any information about any elements in the periodic table. \n\nTo play with me, type in **.play**. Or if you want to retrieve any information, type in **.info [followed by the element name]**. For example: `.info hydrogen` will return you with all the information about the Hydrogen element.";
-	} else if (userCommand.toLowerCase().substring(0, 4) == "info" && userCommand.substring(4, 5) == " ") {
+	} else if (userCommand.substring(0, 4) == "info" && userCommand.substring(4, 5) == " ") {
 		const name = userCommand.substr(5, userCommand.length);
-		reply = info(name.toLowerCase());
+		reply = info(name);
 	} else {
 		reply = "Command Not Found :( Please check for any typo or type in **.help** for more information. "
 	}
@@ -38,21 +47,50 @@ function verify(userCommand) {
 }
 
 function info(elementName) {
-	console.log(elementName);
-	var elementInfo = "";
+
 	try {
-		elementInfo += "**Symbol**: `" + elements[elementName].symbol + "`" + newLine;
-		elementInfo += "**Phase**: " + elements[elementName].phase + newLine;
-		elementInfo += "**Type**: " + elements[elementName].category + newLine;
-		elementInfo += "**Boiling Point**: " + elements[elementName].boil + ", **Melting Point**: " + elements[elementName].melt + newLine;
-		elementInfo += "**Condensed Electron Cofig**: " + elements[elementName].electron_configuration_semantic + newLine;
-		elementInfo += "**Color**: " + elements[elementName].appearance + newLine + newLine;
-		elementInfo += "**Summary**: " + "```" + elements[elementName].summary + "```" + newLine;
+
+		if (elements[elementName].apperance == null) {
+			var app = "none"
+		} else {
+			var app = elements[elementName].apperance
+		}
+
+		const elementEmbed = new MessageEmbed()
+		.setColor('#0099ff')
+		.setTitle('Detailed Element Information')
+		.setURL('https://discord.js.org/')
+		.setAuthor({ name: 'Botnesium', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+		.setDescription('User used `.info`. Showing information on `' + elements[elementName].name + "` element. Symbol: `" + elements[elementName].symbol + "`.")
+		.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+			.addFields(
+			{ name: '\u200B', value: '\u200B' },
+			{ name: 'Summary', value: elements[elementName].summary },
+			{ name: 'Phase', value: elements[elementName].phase, inline: true },
+			{ name: 'Type', value: elements[elementName].category, inline: true },
+			{ name: 'Melting Point (°K)', value: elements[elementName].melt.toString(), inline: true },
+			{ name: 'Boiling Point (°K)', value: elements[elementName].boil.toString(), inline: true },
+			{ name: 'Condensed Electron Config', value: elements[elementName].electron_configuration_semantic, inline: true },
+		)
+		.addField('Apperance', app, true)
+		// .setImage('https://i.imgur.com/AfFp7pu.png')
+		.setTimestamp()
+		.setFooter({ text: 'Contact Hacker3165#3165 with Inquries', iconURL: 'https://raw.githubusercontent.com/TheSagarSaha/SagarSaha01.github.io/master/images/sagar-saha-avatar.jpg' });
+	
+		return ({ embeds: [elementEmbed] });
+
 	} catch (error) {
-		elementInfo = "Opps! Looks like you've made a typo. Please try again. **Make sure you are using the full element name.**";
+		console.log(error);
+		var elementInfo = "Opps! Looks like you've made a typo. Please try again. **Make sure you are using the full element name.**";
 	}
 
 	return elementInfo;
+}
+
+function play() {
+	const randNum = Math.floor(Math.random() * 118);
+	const element = elements[Object.keys(elements)[randNum]].name;
+	console.log(element);
 }
 
 client.login(token);
